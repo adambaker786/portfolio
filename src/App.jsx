ed52e94857a99dd6d944697833a733a967631512
adambaker786 <adambeno@mail.ru>
2022-10-01 18:12:15 +0300

    внес небольшие изменения

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header></header>
+      <Header></Header>
       <main></main>
       <footer></footer>
     </div>
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='header'>
+            <nav className='menu'>
+                <ul>
+                    <li><a href="#about_me">Обо мне</a></li>
+                    <li><a href="#my_projects">Мои проекты</a></li>
+                    <li><a href="my_adress">Мои данные</a></li>
+                </ul>
+            </nav>
         </div>
     );
 };
