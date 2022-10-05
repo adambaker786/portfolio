b0174f5ae7c925bd46626a43d0010c5496fcd1c3
adambaker786 <adambeno@mail.ru>
2022-10-05 22:33:41 +0600

    added some stesk files

--- src/main/projects/Projects.jsx
+++ src/main/projects/Projects.jsx
@@ -5,6 +5,20 @@ const Projects = () => {
         <div className='projects'>
             <h2>PET Projects</h2>
             <p>I have done some interesting pet projects.</p>
+            <div className="steck">
+                <div className="container">
+                     <div className="HTML">HTML</div></div>
+                <div className="container">
+                    <div className="CSS">CSS</div></div>
+                <div className="container">
+                    <div className="JS">JS</div></div>
+                <div className="container">
+                    <div className="React">React</div></div>
+                <div className="container">
+                    <div className="Redux">Redux</div></div>
+                <div className="container">
+                    <div className="figma">Figma</div></div>
+            </div>
         </div>
     );
 };
