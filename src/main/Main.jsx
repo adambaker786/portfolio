f9a510e2e3e6c5a35b1ea37f6c248a9d8d0a4077
adambaker786 <adambeno@mail.ru>
2022-10-10 22:14:45 +0600

    adding clock

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -3,6 +3,9 @@ import ava from '../img/avatar.jpg'
 import Projects from './projects/Projects';
 
 const Main = () => {
+
+    
+
     return (
         <div className='main'>
             <section className='avatarka'>
@@ -11,6 +14,18 @@ const Main = () => {
                 </div>
             </section>
             <Projects></Projects>
+            <div className="clock">
+                <div className="outer-clock">
+                    <div className="marking marking-one"></div>
+                    <div className="marking marking-two"></div>
+                    <div className="marking marking-three"></div>
+                    <div className="marking marking-four"></div>
+                    <div className="marking marking-seconds"></div>
+                    <div className="inner-clock">
+                        <div className="clock-time">12:47</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
