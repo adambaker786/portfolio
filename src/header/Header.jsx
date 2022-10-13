5750a00e66dc5cef4aa557da7f1f804167c98bae
adambaker786 <adambeno@mail.ru>
2022-10-13 19:29:57 +0600

    added header menu

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,13 +4,22 @@ import styles from './Header.module.css'
 const Header = () => {
     return (
         <div className={styles.header}>
-            <nav className='menu'>
-                <ul>
-                    <li><a href="#about_me">Обо мне</a></li>
-                    <li><a href="#my_projects">Мои проекты</a></li>
-                    <li><a href="my_adress">Мои данные</a></li>
-                </ul>
-            </nav>
+            <div className={styles.header_content}>
+                <div className={styles.find_me}>
+                    <div>Fin Me on</div>
+                    <div className={styles.social_icon}></div>
+                    <div className={styles.social_icon}></div>
+                    <div className={styles.social_icon}></div>
+                    <div className={styles.social_icon}></div>
+                </div>
+                <nav className='menu'>
+                    <ul>
+                        <li><a href="#about_me">Обо мне</a></li>
+                        <li><a href="#my_projects">Мои проекты</a></li>
+                        <li><a href="my_adress">Мои данные</a></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 };
