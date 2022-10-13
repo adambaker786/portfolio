579c8ee6fb0195d5060364ba21da4e2a4c0b0eb4
adambaker786 <adambeno@mail.ru>
2022-10-13 19:53:45 +0600

    added some flex in header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import styles from './Header.module.css'
+import whatsapp from '../img/whatsapp.png'
+import facebook from '../img/facebook.png'
+import instagram from '../img/instagram.png'
+import linkedin from '../img/linkedin.png'
 
 const Header = () => {
     return (
@@ -7,10 +11,10 @@ const Header = () => {
             <div className={styles.header_content}>
                 <div className={styles.find_me}>
                     <div>Fin Me on</div>
-                    <div className={styles.social_icon}></div>
-                    <div className={styles.social_icon}></div>
-                    <div className={styles.social_icon}></div>
-                    <div className={styles.social_icon}></div>
+                    <div className={styles.social_icon}><img src={whatsapp} alt='whatsapp icon'/></div>
+                    <div className={styles.social_icon}><img src={facebook} alt='facebook icon'/></div>
+                    <div className={styles.social_icon}><img src={instagram} alt='facebook icon'/></div>
+                    <div className={styles.social_icon}><img src={linkedin} alt='facebook icon'/></div>
                 </div>
                 <nav className='menu'>
                     <ul>
