b0d9e142c30120321295d7468710d251884a91f2
adambaker786 <adambeno@mail.ru>
2022-10-15 10:03:12 +0600

    footer is done

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import copyright from '../img/C.png';
 
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            this is footer
+            <div className={styles.footer_content}>
+                <div className={styles.footer_img}>
+                    <img src={copyright} alt="copyright pictur" />
+                </div>
+                <div className={styles.footer_text}>
+                    Adam Sadulaev
+                </div>
+            </div>
         </div>
     );
 };
