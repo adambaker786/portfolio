56851f5898c78ccb435e295cdd4dad69552645e4
adambaker786 <adambeno@mail.ru>
2022-10-16 12:05:15 +0600

    adding main text description

--- src/main/about/About.jsx
+++ src/main/about/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './About.module.css'
 import ava from '../../img/avatar.jpg'
+import CV from '../../img/CV_Sadulaev.pdf'
 
 const About = () => {
     return (
@@ -13,14 +14,21 @@ const About = () => {
                 </div>
                 <div className={styles.line}></div>
                 <p className={styles.description}>
-                    I'm Adam Sadulaev, a frontend developer. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime perspiciatis eveniet sint iste necessitatibus, vero doloribus repellat nisi perferendis, animi inventore porro, sequi maiores qui praesentium. Vel cum nisi deleniti?
+                    I'm Adam Sadulaev, a frontend developer. 
+                    I love creating websites and find peace in this. I'm interested in horizontal development.
+                    Currently working on PET Projects and every day I'm lerning something new. 
+                    All my Projects are created on React. <br></br>
+                    If you are interested in my portfolio, then I will be happy to take advantage of the opportunity for career growth within your company.
+                    I will be glad to become a part of your team.
                 </p>
                 <div className={styles.download_buttons}>
                     <div className="cv_download">
-                        <button className={styles.cv_download_button}>Download CV</button>
+                        <a href={CV} download>
+                        <button type="button" className={styles.cv_download_button} download>Download CV</button>
+                        </a>
                     </div>
                     <div className={styles.more_info}>
-                        <a className={styles.more_info_button} href="#more">More</a>
+                        <a className={styles.more_info_button} href="https://hh.kz/resume/c020f194ff04fb92b20039ed1f79666e357345?hhtmFrom=resume_list" target="_blank">More</a>
                     </div>
                 </div>
             </div>
